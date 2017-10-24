40de4ab2c2313d1e1524d86aa44b741db89b797e
Instance Noodle <ntn.f51@gmail.com>
2017-10-24 17:45:26 +0700

    add test case for ProgressBar

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -2,6 +2,17 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import ProgressBar from './ProgressBar';
 
-describe('', () => {
+// TODO: need to place those lines wherelse
+var enzyme = require('enzyme');
+var Adapter = require('enzyme-adapter-react-16');
 
+enzyme.configure({ adapter: new Adapter() });
+
+describe('', () => {
+  test('getWidthAsPercentOfTotalWidth should return 50 if the percent is 50 out of the width of 100', () =>{
+    const wrapper = shallow(<ProgressBar percent={50} width={100}/>);
+    const width = wrapper.instance().getWidthAsPercentOfTotalWidth();
+
+    expect(width).toEqual(50);
+  });
 });
