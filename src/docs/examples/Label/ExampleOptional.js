cb96755180434fb336d3b2f02b7653ce65275896
Instance Noodle <ntn.f51@gmail.com>
2017-10-24 11:17:06 +0700

    add Label component

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+/** Optional label */
+const ExampleOptional = () => {
+  return <Label htmlFor="test" label="test" />
+};
+
+export default ExampleOptional;
---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+/** Required label */
+const ExampleRequired = () => {
+  return <Label htmlFor="test" label="test" required />
+};
+
+export default ExampleRequired;
